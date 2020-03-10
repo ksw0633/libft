db746e439cf10c8b4c0fb69ba140ba2da9f17e41
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-10 01:31:27 -0700

    fix

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,13 +6,13 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 10:37:00 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/10 01:20:46 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/10 01:31:14 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t len)
+/*char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	char			*init_dst;
 	unsigned long	i;
@@ -35,4 +35,25 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 		}
 	}
 	return (init_dst);
+}*/
+
+char	*ft_strncpy(char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	int		cpy_end;
+
+	i = 0;
+	cpy_end = 0;
+	while (i < n)
+	{
+		if (s2[i] && !cpy_end)
+			s1[i] = s2[i];
+		else
+		{
+			s1[i] = '\0';
+			cpy_end = 1;
+		}
+		i++;
+	}
+	return (s1);
 }
