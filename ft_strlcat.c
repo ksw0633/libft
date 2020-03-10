ac6a1c58cb4c17f782025bd361eb25b682dd3c56
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-09 21:26:38 -0700

    fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 16:57:45 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/09 16:18:12 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/09 21:26:22 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	unsigned long	i;
 	size_t			return_value;
 
-	if (dst && src)
-	{
 		i = 0;
 		return_value = ft_strlen(dst) + ft_strlen(src);
 		if (!dstsize || ft_strlen(dst) > dstsize)
@@ -35,7 +33,4 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		}
 		*dst = 0;
 		return (return_value);
-	}
-	else
-		return (0);
 }
