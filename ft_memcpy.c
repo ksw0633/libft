3fc8b2fc0c18462d9e69ddf345385931d383e40c
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-09 21:59:05 -0700

    fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/23 14:12:38 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/05 16:22:21 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/09 21:58:42 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	void *init_dst;
 	char *temp;
 	char *source;
-
+	
+	if (n > sizeof(src))
+		n = sizeof(src);
 	init_dst = dst;
 	temp = (char *)dst;
 	source = (char *)src;
