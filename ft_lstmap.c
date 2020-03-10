8ed15a3a3c8c9f0d5df68d4525e865b71de9359c
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-09 21:42:18 -0700

    fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 20:20:30 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/08 21:44:11 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/09 21:41:38 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 		return (current);
 	return_value = current;
 	lst = lst->next;
-	while (lst)
+	while (lst != NULL)
 	{
 		temp = f(lst);
 		if (!(current->next = ft_lstnew(temp->content, temp->content_size)))
