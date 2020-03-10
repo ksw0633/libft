31279413a8250dfb7761e7f4fc37103b91cfb11f
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-10 01:29:57 -0700

    fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,36 +6,30 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 20:38:07 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/10 00:35:33 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/10 01:29:49 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void    *ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*buffer;
-	char	*destination;
-	char	*source;
-	size_t	init_len;
+    unsigned int i;
 
-	if (!dst || !src)
-		return (NULL);
-	init_len = 0;
-	destination = (char *)dst;
-	source = (char *)src;
-	buffer = (char *)malloc(len);
-	if (!buffer)
-		return (NULL);
-	while (init_len < len)
-	{
-		buffer[(int)init_len] = source[(int)init_len];
-		init_len++;
-	}
-	while (init_len - len < len)
-	{
-		destination[(int)(init_len - len)] = buffer[(int)(init_len - len)];
-		init_len++;
-	}
-	return (dst);
+    if (!dst || !src)
+        return (NULL);
+    i = 0;
+    if (((unsigned char *)src) < ((unsigned char *)dst))
+        while (len > 0)
+        {
+            len--;
+            ((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+        }
+    else
+        while (i < len)
+        {
+            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+    return (((unsigned char *)dst));
 }
