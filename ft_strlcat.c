e45ee76854277a406157e58fa99d6f52c53f2b70
Sungwoo Kim <sunkim@e1z3r7p15.42.us.org>
2020-03-09 22:37:55 -0700

    fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sunkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 16:57:45 by sunkim            #+#    #+#             */
-/*   Updated: 2020/03/09 21:26:22 by sunkim           ###   ########.fr       */
+/*   Updated: 2020/03/09 22:37:40 by sunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	unsigned long	i;
 	size_t			return_value;
 
-		i = 0;
-		return_value = ft_strlen(dst) + ft_strlen(src);
-		if (!dstsize || ft_strlen(dst) > dstsize)
-			return (ft_strlen(src) + dstsize);
-		dstsize -= ft_strlen(dst);
-		while (*dst)
-			dst++;
-		while (i < dstsize - 1 && *src)
-		{
-			*dst = *src;
-			dst++;
-			src++;
-			i++;
-		}
-		*dst = 0;
-		return (return_value);
+	if (!dst || !src || !dstsize)
+		return (0);
+	i = 0;
+	return_value = ft_strlen(dst) + ft_strlen(src);
+	if (!dstsize || ft_strlen(dst) > dstsize)
+		return (ft_strlen(src) + dstsize);
+	dstsize -= ft_strlen(dst);
+	while (*dst)
+		dst++;
+	while (i < dstsize - 1 && *src)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+		i++;
+	}
+	*dst = 0;
+	return (return_value);
 }
